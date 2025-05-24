a377521c4f8516e13a87895d217e2a592231fe4b
RuanRidleySenai <ruan.r.lima6@aluno.senai.br>
2025-05-23 23:42:25 -0300

    Introdução de Classes e construtores

---
+++ CursoJava/Classe/Jogador.java
@@ -0,0 +1,8 @@
+package CursoJava.Classe;
+
+public class Jogador {
+    public Jogador(int n){
+        System.out.printf("Jogador de número %d criado", n); // imprime na tela quando o objeto é instanciado
+    }
+    
+}
---
+++ CursoJava/Classe/Time.java
@@ -0,0 +1,12 @@
+package CursoJava.Classe;
+
+public class Time{
+   public static void main(String[] args) {
+
+    Jogador jogador1 = new Jogador(10); // instanciando a classe Jogador;
+    
+   }
+}
+
+// Classe é um molde, um modelo, uma estrutura que define as propriedades e comportamentos de um objeto. (de grosso modo, uma classe é uma representação de um objeto)
+// metodo construtor - é um método especial que é chamado quando um objeto é criado a partir de uma classe. O método construtor tem o mesmo nome da classe e não tem tipo de retorno. Ele é usado para inicializar os atributos do objeto e executar qualquer outra lógica necessária durante a criação do objeto.
